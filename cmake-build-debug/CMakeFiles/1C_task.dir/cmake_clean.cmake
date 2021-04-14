256f1a8561e2a2126008e7cc0e1b0551165869b2
ea-evdokimov <evdokimov.ea@phystech.edu>
2021-04-14 11:07:38 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/1C_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1C_task"
+  "1C_task.pdb"
+  "CMakeFiles/1C_task.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1C_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main() {
+    std::cout << "Hi, 1C!" << std::endl;
+    return 0;
+}
